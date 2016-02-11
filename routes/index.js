7846ab657751cef103730cd5b6d700e568040d64
Jonathan Tobin <jonny.tobin24@gmail.com>
2016-02-11 16:10:15 +0000

    Edited the index.js file

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function (req, res) {
+    console.log(req.body.title);
+    console.log(req.body.description);
+    res.send('Post page');
+});
+
 module.exports = router;
