d3231a36befd7ad01f9ec7d8249cdcadf26ebccf
Jonathan Tobin <jonny.tobin24@gmail.com>
2016-02-11 20:14:56 +0000

    Added an input form to the index.jade page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/secrets', function(req, res, next) {
+  // SELECT password from DB where username='req.body.yourName'
+
+  if (req.body.Password == "password") {
+    res.render('/', {Name: req.body.Name, pwd: req.body.Password});
+  }
+  else {
+      res.redirect('/index?Name=Tom');
+  }
+});
+
 module.exports = router;
