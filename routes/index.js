63181ec552cc852fcb5498d38feb3bc9bbc8247a
Jonathan Tobin <jonny.tobin24@gmail.com>
2016-02-29 17:54:42 +0000

    Added the ability tosign in using a prededifined password

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// If we get a GET request for /name
+router.get('/secrets', function(req, res, next) {
+  res.render('secrets');
+});
+
+// If we get a POST request for /name
 router.post('/secrets', function(req, res, next) {
   // SELECT password from DB where username='req.body.yourName'
 
   if (req.body.Password == "password") {
-    res.render('/', {Name: req.body.Name, pwd: req.body.Password});
+    res.render('secrets', {Name: req.body.Name, Password: req.body.Password});
   }
   else {
       res.redirect('/index?Name=Tom');
