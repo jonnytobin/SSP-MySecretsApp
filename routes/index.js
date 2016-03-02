e2413cc5782a7de17616ba075b6f69b829cf6bea
Jonathan Tobin <jonny.tobin24@gmail.com>
2016-03-02 11:37:32 +0000

    Added some commenting

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
+// This is an appplication to allow the user to store
+// their secrets. To log in use:
+//                              Username: username
+//                              Password: password
+
 var express = require('express');
 var router = express.Router();
+// Creating an array to store the secrets
 var list = [];
 
 /* GET home page. */
@@ -14,6 +20,7 @@ router.get('/secrets', function (req, res, next) {
 
 // If we get a POST request for /secrets
 router.post('/secrets', function (req, res, next) {
+    // If passward is equal to"password"
     if (req.body.Password == "password") {
         res.render('secrets', { Name: req.body.Name, Password: req.body.Password });
         getText();
@@ -24,6 +31,8 @@ router.post('/secrets', function (req, res, next) {
     console.log(list)
 });
 
+// Trying to get the text form the textfield (secret) and 
+// add it to the list array
 function getText() {
     var str = document.getElementById("txt");
     list.push(str.value);
@@ -36,8 +45,5 @@ function getText() {
         console.log(list[i]);
     }
 }
-
-
-
 
 module.exports = router;
